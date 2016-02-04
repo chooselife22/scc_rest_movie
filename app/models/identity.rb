9e3cb0c7e0db38c8e63a03d99f87df03744ff157
Andy Schreiter <andy.schreiter@mailbox.tu-dresden.de>
2016-02-04 10:18:34 +0100

    Identity validation nur wenn password gesetzt ist

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,6 +1,8 @@
 class Identity < ActiveRecord::Base
-  belongs_to :user
   has_secure_password
+
+  attr_accessor :password, :password_confirmation
+  validates :password, length: (1..32), confirmation: true, if: :setting_password?
 
   def self.from_google_oauth2(auth)
     identity = Identity.where(uid: auth["uid"], provider: "google_plus").first_or_initialize do |i|
@@ -8,5 +10,21 @@ class Identity < ActiveRecord::Base
     end
     identity.save validate: false
     identity
+  end
+
+  def password=(password_str)
+    @password = password_str
+    self.password_salt   = BCrypt::Engine.generate_salt
+    self.password_digest = BCrypt::Engine.hash_secret(password_str, password_salt)
+  end
+
+  def authenticate(password)
+    password.present? && password_digest.present? && password_digest == BCrypt::Engine.hash_secret(password, password_salt)
+  end
+
+  private
+
+  def setting_password?
+    self.password || self.password_confirmation
   end
 end
